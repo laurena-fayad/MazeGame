04e288299e3fa23a876d2b9b471a66a23c6eaae3
Laurena Fayad <laurenafayad@gmail.com>
2022-01-22 19:58:51 +0200

    Make all maze borders red over hover & S resets color to black

--- game.js
+++ game.js
@@ -1,17 +1,20 @@
 window.onload = function(){
-    let maze_borders = document.getElementsByClassName('Boundary');
+    let maze_borders = document.getElementById('game').getElementsByClassName('boundary');
     let maze_border1 = document.getElementById('boundary1');
     let start_btn = document.getElementById('start');
   
     start_btn.addEventListener("click", function() {
+        //Resets maze color if it is red
 
-        if(maze_border1.style.borderColor = "red")
-        {
-            maze_border1.style.borderColor = "black";
+        for (var i=0; i<maze_borders.length; i++){
+            if(maze_borders[i].style.borderColor = "red"){
+               maze_borders[i].style.borderColor = "black";
+            }
+            maze_borders[i].addEventListener("mouseover", function(){
+                for (var i=0; i<maze_borders.length; i++){
+                    maze_borders[i].style.borderColor = "red";
+                }
+            })
         }
-
-        maze_border1.addEventListener("mouseover", function(){
-            maze_border1.style.borderColor = "red";
-        })
     })
 }
