1dc998a23d9f0e01592bf6f372173a2a3af91409
Laurena Fayad <laurenafayad@gmail.com>
2022-01-22 20:23:27 +0200

    Update status text when the player loses

--- game.js
+++ game.js
@@ -1,18 +1,22 @@
 window.onload = function(){
     let maze_borders = document.getElementById('game').getElementsByClassName('boundary');
-    let maze_border1 = document.getElementById('boundary1');
     let start_btn = document.getElementById('start');
+    let loser = "You lose! Move your mouse over the S to try again.";
+    let status = document.getElementById('status');
+    let initial_status = "Avoid touching the walls... ";
   
-    start_btn.addEventListener("click", function() {
+    start_btn.addEventListener("mouseover", function() {
         //Resets maze color if it is red
 
         for (var i=0; i<maze_borders.length; i++){
             if(maze_borders[i].style.borderColor = "red"){
                maze_borders[i].style.borderColor = "black";
+               status.innerHTML= initial_status;
             }
             maze_borders[i].addEventListener("mouseover", function(){
                 for (var i=0; i<maze_borders.length; i++){
                     maze_borders[i].style.borderColor = "red";
+                    document.getElementById('status').innerHTML= loser;
                 }
             })
         }
