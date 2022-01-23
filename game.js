2f3b067d47c382350a2bef7a3b477334c59f22aa
Laurena Fayad <laurenafayad@gmail.com>
2022-01-23 12:03:31 +0200

    Added a youwin function

--- game.js
+++ game.js
@@ -2,12 +2,8 @@ window.onload = function(){
     let maze_borders = document.getElementById('game').getElementsByClassName('boundary')
     let start_btn = document.getElementById('start')
     let end_btn = document.getElementById('end')
-    let loser = "You lost! Move your mouse over the S to try again."
-    let winner = "Congratulations, you won! Move your mouse over the S to play again."
     let status = document.getElementById('status')
     let game_status = "Avoid touching the walls... "
-    let initial_status = "Begin by moving your mouse over the S."
-
     
     function colorWallsRed(){
         for (var i = 0; i<maze_borders.length; i++){
@@ -21,21 +17,35 @@ window.onload = function(){
         }
     }
 
-    function checkRedWalls(){
-        if (document.getElementById('boundary1').style.backgroundColor = "red"){
-            return true;
+    function colorWallsGreen(){
+        for (var i = 0; i<maze_borders.length; i++){
+            maze_borders[i].style.backgroundColor = "green";
         }
     }
 
-    start_btn.addEventListener("onclick", colorWallsGray)
+    function resetMaze(){
+        colorWallsGray()
+        status.innerHTML = game_status
+    }
+
+    function youLose(){
+        colorWallsRed()
+        status.innerHTML = "You lost! Score = -10"
+    }
+
+    function youWin(){
+        if (document.getElementById('boundary1').style.backgroundColor = "#eeeeee"){
+            colorWallsGreen()
+            status.innerHTML = "Great job, you won! Score = 5"
+        }
+    }
 
     start_btn.addEventListener("mouseover", function(){
-        if (checkRedWalls()){
-            colorWallsGray();
+        
+        for (var i = 0; i<maze_borders.length; i++){
+            maze_borders[i].addEventListener("mouseover", youLose)
         }
+        start_btn.addEventListener("click", resetMaze)
+        end_btn.addEventListener("mouseover", youWin)
     })
-
-    for (var i = 0; i<maze_borders.length; i++){
-        maze_borders[i].addEventListener("mouseover", colorWallsRed)
-    }
 }
