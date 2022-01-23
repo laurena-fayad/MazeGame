f9e9acc8381a146599b82b27f330556c204f9f8c
Laurena Fayad <laurenafayad@gmail.com>
2022-01-23 11:14:39 +0200

    created atomic functions

--- game.js
+++ game.js
@@ -1,24 +1,41 @@
 window.onload = function(){
-    let maze_borders = document.getElementById('game').getElementsByClassName('boundary');
-    let start_btn = document.getElementById('start');
-    let loser = "You lose! Move your mouse over the S to try again.";
-    let status = document.getElementById('status');
-    let initial_status = "Avoid touching the walls... ";
-  
-    start_btn.addEventListener("mouseover", function() {
-        //Resets maze color if it is red
+    let maze_borders = document.getElementById('game').getElementsByClassName('boundary')
+    let start_btn = document.getElementById('start')
+    let end_btn = document.getElementById('end')
+    let loser = "You lost! Move your mouse over the S to try again."
+    let winner = "Congratulations, you won! Move your mouse over the S to play again."
+    let status = document.getElementById('status')
+    let game_status = "Avoid touching the walls... "
+    let initial_status = "Begin by moving your mouse over the S."
 
-        for (var i=0; i<maze_borders.length; i++){
-            if(maze_borders[i].style.borderColor = "red"){
-               maze_borders[i].style.borderColor = "black";
-               status.innerHTML= initial_status;
-            }
-            maze_borders[i].addEventListener("mouseover", function(){
-                for (var i=0; i<maze_borders.length; i++){
-                    maze_borders[i].style.borderColor = "red";
-                    document.getElementById('status').innerHTML= loser;
-                }
-            })
+    
+    function colorWallsRed(){
+        for (var i = 0; i<maze_borders.length; i++){
+            maze_borders[i].style.backgroundColor = "red";
+        }
+    }
+
+    function colorWallsGray(){
+        for (var i = 0; i<maze_borders.length; i++){
+            maze_borders[i].style.backgroundColor = "#eeeeee";
+        }
+    }
+
+    function checkRedWalls(){
+        if (document.getElementById('boundary1').style.backgroundColor = "red"){
+            return true;
+        }
+    }
+
+    start_btn.addEventListener("onclick", colorWallsGray)
+
+    start_btn.addEventListener("mouseover", function(){
+        if (checkRedWalls()){
+            colorWallsGray();
         }
     })
+
+    for (var i = 0; i<maze_borders.length; i++){
+        maze_borders[i].addEventListener("mouseover", colorWallsRed)
+    }
 }
